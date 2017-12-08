7e6036d7313e49ca18f1943297806f47374f8dec
Aleeya Rasool <31712857+aleeyarasool@users.noreply.github.com>
2017-12-07 19:43:31 -0500

    Exercise 3

--- Python Exercises/Exercise3.py
+++ Python Exercises/Exercise3.py
@@ -1,68 +1,23 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
+#Explain the output
 float(123)
-
-
-# In[2]:
-
 
 float('123')
 
-
-# In[3]:
-
-
 float('123.23')
-
-
-# In[4]:
-
 
 int(123.23)
 
-
-# In[5]:
-
-
 int('123.23')
-
-
-# In[6]:
-
 
 int(float('123.23'))
 
-
-# In[7]:
-
-
 str(12)
-
-
-# In[8]:
-
 
 str(12.2)
 
-
-# In[9]:
-
-
 bool('a')
 
-
-# In[10]:
-
-
 bool(0.1)
-
-
-# In[11]:
-
 
 bool(0)
 
