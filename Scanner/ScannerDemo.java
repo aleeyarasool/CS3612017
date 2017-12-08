bac2e6db4277981206444433a9bdfd406b794dd6
Aleeya Rasool <31712857+aleeyarasool@users.noreply.github.com>
2017-12-07 19:34:47 -0500

    Scanner for Jay

---
+++ Scanner/ScannerDemo.java
@@ -0,0 +1,25 @@
+/**
+ * @author Christelle
+ * 
+ */
+import java.io.*;
+import java.util.*;
+public class ScannerDemo {
+
+	private static String file = "C:\\Users\\Aleeya\\workspace\\ParserScannerOriginal\\src\\prog2.jay";  //change this file path for each file run
+	private static int counter = 1;
+
+	public static void main(String args[]) throws Exception {
+
+		TokenStream ts = new TokenStream(file);
+
+		Token t;
+
+		while (!ts.isEndofFile()) {
+			t = ts.nextToken();
+			t.toString();
+			System.out.println("Token " + counter + " - " + t);
+			counter  = counter+1;
+		}
+	}
+}
